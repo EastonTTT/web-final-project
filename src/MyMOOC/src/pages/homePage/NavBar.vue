2124f32240405ee24aa92c6e5f1a6fc550545e61
EastonTTT <1074908183@qq.com>
2024-12-05 12:12:36 +0800

    更新登录按钮与头像

--- src/MyMOOC/src/pages/homePage/NavBar.vue
+++ src/MyMOOC/src/pages/homePage/NavBar.vue
@@ -14,11 +14,14 @@
         alt="Element logo"
       />
     </el-menu-item>
-
+    <!-- 表项 -->
     <el-menu-item index="1"><el-text class="mx-1" size="large">首页</el-text></el-menu-item>
-    <el-menu-item index="2"><el-text class="mx-1" size="large">课程</el-text></el-menu-item>
+
+    <el-menu-item index="2"><nav-dropdown/></el-menu-item>
+
     <el-menu-item index="3"><el-text class="mx-1" size="large">关于我们</el-text></el-menu-item>
 
+    <!-- 搜索框 -->
     <el-input
       v-model="searchInput"
       style="width: 240px;height:40px;margin-top: 10px;"
@@ -27,8 +30,8 @@
       :prefix-icon="Search"
     />
     <el-button type="primary" :icon="Search" size="small" style="margin:auto 10px">Search</el-button>
-
-    <Avatar></Avatar>
+    <div class="my-button"><el-button type="primary" v-if="!ifLogin">登录</el-button></div>
+    <Avatar v-if="ifLogin"></Avatar>
   </el-menu>
 </template>
 
@@ -36,6 +39,7 @@
 import { ref } from 'vue'
 import { Search } from '@element-plus/icons-vue'
 import Avatar from './Avatar.vue';
+import NavDropdown from './NavDropdown.vue';
 
 const activeIndex = ref('1')
 const handleSelect = (key: string, keyPath: string[]) => {
@@ -43,6 +47,8 @@ const handleSelect = (key: string, keyPath: string[]) => {
 }
 // 搜索框
 const searchInput = ref('')
+
+const ifLogin = ref(false)
 </script>
 
 <style>
@@ -51,5 +57,8 @@ const searchInput = ref('')
 }
 .el-menu-item{
   font-size: 20px;
+}
+.my-button{
+  margin: 12px 5px;
 }
 </style>
