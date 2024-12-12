a13996ccf228f38a16b298cde589562b520ea420
EastonTTT <1074908183@qq.com>
2024-12-12 22:01:18 +0800

    登录校验前后端完成，联调成功。

--- src/MyMOOC/src/pages/homePage/login/LoginForm.vue
+++ src/MyMOOC/src/pages/homePage/login/LoginForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="width: 350px">
-    <t-form ref="form" :data="formData" :colon="true" :label-width="0" @reset="onReset" @submit="onSubmit" >
+    <t-form ref="form" :data="formData" :colon="true" :rules="rules" :label-width="0" @reset="onReset" @submit="onSubmit" >
       <t-form-item name="account">
         <t-input v-model="formData.account" clearable placeholder="请输入账户名">
           <template #prefix-icon>
@@ -27,28 +27,92 @@
 </template>
 <script lang="ts" setup>
 import { reactive } from 'vue';
-import { MessagePlugin, FormProps } from 'tdesign-vue-next';
+import { MessagePlugin, FormProps, type FormRule } from 'tdesign-vue-next';
 import { DesktopIcon, LockOnIcon } from 'tdesign-icons-vue-next';
 import { useRouter } from 'vue-router';
-import { defineEmits } from 'vue';
+// import { defineEmits } from 'vue';
 import { LogStatus } from '@/pages/homePage/login/LogStatus';
-
+import { LoginRecord } from '@/pages/homePage/login/LoginRecord';
+import MyAxios from '@/utils/request/Axios.ts';
 
 const router = useRouter()
 const emit = defineEmits(['close-dialog']);
+
+//登录表单内置校验方法:
+const rules: FormProps['rules'] = {
+  account:[
+    {
+      required: true,
+      message: '姓名必填',
+      type: 'error',
+      trigger: 'blur',
+    },
+    {
+      required: true,
+      message: '姓名必填',
+      type: 'error',
+      trigger: 'change',
+    },
+    {
+      whitespace: true,
+      message: '姓名不能为空',
+    },
+    {
+      max: 15,
+      message: '最大长度不能超过15个字符',
+      type: 'error',
+      trigger: 'blur',
+    }
+  ],
+  password:[
+  {
+      required: true,
+      message: '密码必填',
+      type: 'error',
+      trigger: 'blur'
+    },
+    {
+      required: true,
+      message: '密码必填',
+      type: 'error',
+      trigger: 'change'
+    },
+  ]
+}
+
+//登陆数据表单数据模型
 const formData: FormProps['data'] = reactive({
   account: '',
   password: '',
 });
+
 const onReset: FormProps['onReset'] = () => {
   MessagePlugin.success('重置成功');
 };
-const onSubmit: FormProps['onSubmit'] = ({ validateResult, firstError }) => {
+
+const onSubmit: FormProps['onSubmit'] = async ({ validateResult, firstError }) => {
+
+
   if (validateResult === true) {
-    MessagePlugin.success('登录成功')
-    emit('close-dialog');
-    LogStatus.isLogged = true;
-    // router.push('/user');
+    try{
+      const response = await MyAxios.myPosting('/login',
+        {
+          username: formData.account,
+          password: formData.password
+        });
+        if(response.data.status === 200){
+          MessagePlugin.success('登录成功')
+          emit('close-dialog');
+          LogStatus.isLogged = true;
+          LoginRecord.username = response.data.data.username;
+          LoginRecord.role = response.data.data.role;
+        }else{
+          MessagePlugin.error('登陆失败：' + response.data.message)
+        }
+    }catch(error){
+      console.log('登录请求失败',error)
+      MessagePlugin.error('登录请求失败')
+    }
   } else {
     console.log('Validate Errors: ', firstError, validateResult);
     MessagePlugin.warning(firstError);
